3e22fa705f1df2746aae44a727c93386cc96ead4
Maximiliano Levi <maxilevi77@gmail.com>
2022-11-16 01:08:54 -0300

    add topo

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # firewall_mininet
+
+## como correr
+
+#### correr pox
+`python3 ./pox.py forwarding.l2_learning`
+
+#### correr mininet
+
+`sudo mn --custom src/topo.py --topo customTopo --mac --arp --controller remote`
--- src/topo.py
+++ src/topo.py
@@ -5,18 +5,25 @@ class Topologia(Topo):
 
     def __init__(self, switch_count):
         assert switch_count > 0
+        self.switch_count = switch_count
         Topo.__init__(self)
 
-        h1 = self.addHost('host_1')
-        h2 = self.addHost('host_2')
+    def build(self):
+        h1_1 = self.addHost('h1_1')
+        h1_2 = self.addHost('h1_2')
+        h2_1 = self.addHost('h2_1')
+        h2_2 = self.addHost('h2_2')
 
         switches = []
-        for i in range(switch_count):
+        for i in range(self.switch_count):
             switches.append(self.addSwitch(f'switch_{i}'))
 
-        self.addLink(h1, switches[0])
-        self.addLink(switches[-1], h2)
+        self.addLink(h1_1, switches[0])
+        self.addLink(h1_2, switches[0])
+        self.addLink(switches[-1], h2_1)
+        self.addLink(switches[-1], h2_2)
 
-        for i in range(1, switch_count-1):
+        for i in range(1, self.switch_count):
             self.addLink(switches[i-1], switches[i])
 
+topos = {'customTopo' : lambda: Topologia(2) }
